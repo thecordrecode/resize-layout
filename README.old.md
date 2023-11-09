2e10e1a78c3f7a90c5c496263dea58ba535cb2ae
The_cord_recode <110207163+thecordrecode@users.noreply.github.com>
2023-11-09 19:45:35 +0900

    1
    1

---
+++ README.old.md
@@ -0,0 +1,8 @@
+# resize-layout
+resize layout on environment that is created on css
+
+## RESIZE-LAYOUT 
+Css interfaces that you can control component size and locate 
+
+## How to start? 
+
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ResizeLayout from './resize-layout/js/resize-layout';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ResizeLayout/>
+  </React.StrictMode>
+);
+
